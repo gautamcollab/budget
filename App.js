cbefae82680b6af6167a1b039e8efe4bc0f0f33a
Gautam Sinha <gautam.sinha@live.com>
2022-06-21 16:39:17 -0400

    added models

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { Button, View, Text, StyleSheet, TouchableOpacity } from 'react-native';
+import { Button, View, Text, StyleSheet, TouchableOpacity, TextComponent } from 'react-native';
 import { NavigationContainer, TabActions } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
@@ -9,29 +9,12 @@ import { Camera, CameraType } from 'expo-camera';
 const Stack = createNativeStackNavigator();
 
 function App() {
-
-
-
-  // React.useEffect(() => {
-  //   checkCameraPermission();
-  // }, []);
-
   return (
-    // <NavigationContainer>
-    //   <Stack.Navigator>
-    //     <Stack.Screen name="Home" component={HomeScreen} />
-    //     <Stack.Screen name="Details" component={DetailsScreen} />
-    //     <Stack.Screen name="HomeText" component={HomeText} /> 
-    //   </Stack.Navigator>
-    // </NavigationContainer>
     <NavigationContainer>
       <BottomTabs />
     </NavigationContainer>
-  
   );
 }
-
-
 
 const Tab = createBottomTabNavigator(); 
 
@@ -42,35 +25,106 @@ function BottomTabs() {
       <Tab.Screen name="Budget" component={BudgetScreen} />
       <Tab.Screen name="Camera" component={Photo} /> 
     </Tab.Navigator>
-
   );
 }
-
 
 function HomeScreen() {
   return (
     <>
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Home Screen</Text>
+      <Text style={{fontSize: 30, marginTop: 1}}>Home Screen</Text>
     </View>
     </>
   );
 }
 
+// ----------- Budget page
+
 function BudgetScreen({ navigation }) {
+
+  var currentBudget = new Budget();
+  currentBudget.initialAmount = 200;
+  currentBudget.expenses = [];
+  currentBudget.expenses.push(
+    {
+      description: "Test",
+      amount: 25
+    }
+  );
+
+
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Budget</Text>
+    <View style={ budgetStyles.mainContainer }>
      
+        {/* Budget entry and container */}
+        <View style={{ alignContent: 'center', alignItems: 'center', borderBottomColor: "#CCC", borderBottomWidth: "2px", margin: "5%" }}>
+          <Text style={budgetStyles.budgetContainer}>$ {currentBudget.initialAmount} </Text>
+        </View>
+        
+        {/* Expenses */}
+        <View>
+          <Text style={budgetStyles.expenseDescriptionContainer}>
+          {
+            currentBudget.expenses[0].description
+          }  
+          </Text>          
+          <View style={{ alignItems: 'right', alignContent: 'right', alignSelf: 'right'}}>
+            <Text style={budgetStyles.expenseAmountContainer}>
+              $ 
+              {
+                currentBudget.expenses[0].amount
+              }
+            </Text>
+          </View>
+          
+        </View>
+
+        {/* Total */}
+        <View style={{ alignContent: 'center', alignItems: 'center', borderTopColor: "#CCC", borderTopWidth: "2px", margin: "5%" }}>
+          <Text style={budgetStyles.totalContainer}>$ 65.00 </Text>
+        </View>
+        
         <Button title="Go back" onPress={() => navigation.goBack()}
         />
     </View>
   );
 }
 
+const budgetStyles = StyleSheet.create({
+  mainContainer: {
+    justifyContent: 'center',
+    // flexDirection: "row"
+  },
+  budgetContainer: {
+    fontSize: 64,
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 20,
+    borderBottomWidth: "1px",
+    borderBottomColor: "#FFF"
+ 
+  },
+  totalContainer: {
+    color: "#CCC",
+    fontSize: 56
+  },
+  expenseAmountContainer: {
+    fontSize: 34,
+    marginLeft: 16,
+    alignSelf: 'right',
+    // flex: 3
+  },
+  expenseDescriptionContainer: {
+    marginLeft: 16,
+    fontSize: 34,
+    color: "red",
+  }
+});
 
-function Photo({ navigation }) {
-  const [hasPermission, setHasPermission] = useState(null);
+
+// -------------
+
+function Photo() {
   const [type, setType] = useState(CameraType.back);
   useEffect(() => {
     (async () => {
@@ -86,9 +140,8 @@ function Photo({ navigation }) {
             <TouchableOpacity
               style={styles.button}
               onPress={() => {
-                setType(type === CameraType.back ? CameraType.front : CameraType.back);
+                setType(type === CameraType.front);
               }}>
-              <Text style={styles.text}> Flip </Text>
             </TouchableOpacity>
           </View>
         </Camera>
@@ -120,6 +173,17 @@ function Photo({ navigation }) {
   });
 
 
+  // ------------- Models ---------
+  class Budget{
+    initialAmount;
+    expenses
+  }
+  
+  class Expense{
+    description;
+    amount;
+  }
+  
 
 
 export default App;
